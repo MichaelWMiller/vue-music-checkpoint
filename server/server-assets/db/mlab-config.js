f0cb45854e1b1856d65a503c0f1c41c21eb44b3d
mwmiller75 <mwmiller75@gmail.com>
2018-03-01 16:30:20 -0700

    initial check-in

--- server/server-assets/db/mlab-config.js
+++ server/server-assets/db/mlab-config.js
@@ -1,14 +1,14 @@
 var mongoose = require("mongoose");
-console.log('Be sure to put in your connection string!')
+console.log('mongodb://test:test@ds044907.mlab.com:44907/mwm-db')
 var connectionString = "";
 
 var connection = mongoose.connection;
 mongoose.connect(connectionString);
 
 connection.on("error", err => {
-  console.error("mlab Error: ", err);
+    console.error("mlab Error: ", err);
 });
 
 connection.once("open", () => {
-  console.log("connected to database");
+    console.log("connected to database");
 });
