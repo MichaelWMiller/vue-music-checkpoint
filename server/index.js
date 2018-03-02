04e0bdfd7abdb5991e3d9674d42c75da91bb2c0c
mwmiller75 <mwmiller75@gmail.com>
2018-03-01 17:11:58 -0700

    Had to rebuild public dir to get server to run

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 let express = require("express"),
-  bp = require("body-parser"),
-  cors = require("cors"),
-  server = express(),
-  port = 3000;
+    bp = require("body-parser"),
+    cors = require("cors"),
+    server = express(),
+    port = 3000;
 
 require("./server-assets/db/mlab-config");
+var whitelist = ['http://localhost:8080']
+var corsOptions = {
+    origin: function(origin, callback) {
+        var originIsWhiteListed = whitelist.indexOf(origin) !== -1
+        callback(null, originIsWhiteListed)
+    },
+    credentials: true
+}
 
 server.use(cors());
 server.use(bp.json());
@@ -15,8 +23,8 @@ server.use(bp.urlencoded({ extended: true }));
 
 server.use("*", (error, req, res, next) => {
     res.status(400).send(error);
-  });
-  
+});
+
 server.listen(port, () => {
     console.log("the server is running... Port:", port);
-  });
+});
